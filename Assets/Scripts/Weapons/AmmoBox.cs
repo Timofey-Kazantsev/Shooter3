7c7bbd85b80a3e0e32660ddcdbcf932e01d6a2f4
Timofey-Kazantsev <93885215+Timofey-Kazantsev@users.noreply.github.com>
2024-11-02 19:02:06 +0300

    Ammo

--- Assets/Scripts/Weapons/AmmoBox.cs
+++ Assets/Scripts/Weapons/AmmoBox.cs
@@ -6,51 +6,104 @@ using XtremeFPS.WeaponSystem;
 
 public class AmmoBox : MonoBehaviour
 {
-    [SerializeField] private int ammoInBox = 100; // Количество гранат в ящике
-    [SerializeField] private KeyCode interactKey = KeyCode.F; // Клавиша для взаимодействия
+    [SerializeField] private int ammoInBox = 100; 
+    [SerializeField] private KeyCode interactKey = KeyCode.F;    
     [SerializeField] private float interactionDistance = 3f;
     private int bulletsToAdd;
+    private int ammoInBoxForAK;
+    private int ammoInBoxForShotgun;
+    private int ammoInBoxForPistol;
 
+    void Start()
+    {
+        ammoInBoxForAK = ammoInBox;
+        ammoInBoxForPistol = ammoInBox;
+        ammoInBoxForShotgun = ammoInBox;
+    }
     // Update is called once per frame
     void Update()
     {
         GameObject player = GameObject.FindWithTag("Player");
         GameObject gun = GameObject.FindGameObjectWithTag("Gun");
+        GameObject weaponSlot = GameObject.Find("Player Parent/Player Armature/Camera Holder/Camera Follow/Weapon Holder/Weapon Recoils");
+        UniversalWeaponSystem gunSystem = weaponSlot.transform.GetChild(0).GetComponent<UniversalWeaponSystem>();
 
         if (player != null && Vector3.Distance(transform.position,
             player.transform.position) <= interactionDistance && gun != null)
         {
-            UniversalWeaponSystem gunSystem = gun.GetComponent<UniversalWeaponSystem>();
             
-           
             if (Input.GetKeyDown(interactKey) && gunSystem != null)
             {
-                GameObject weaponSlot = GameObject.Find("Player Amature/Camera Holder/Camera Follow/Weapon Holder/Weapon Recoils");
-                Debug.Log(weaponSlot.name);
-                UniversalWeaponSystem gunSys = weaponSlot.transform.GetChild(0).GetComponent<UniversalWeaponSystem>();
                 if (ammoInBox > 0 && gunSystem.totalBullets < gunSystem.maxAmmo)
                 {
                     bulletsToAdd = gunSystem.maxAmmo - gunSystem.totalBullets;
-                    if (ammoInBox <= bulletsToAdd)
+                    if (gunSystem.name == "AK-47")
                     {
-                        bulletsToAdd = ammoInBox;
-                        ammoInBox -= bulletsToAdd;
-                        gunSystem.addAmmo(bulletsToAdd);
+                        if(ammoInBoxForAK > 0)
+                        {
+                            if (ammoInBoxForAK <= bulletsToAdd)
+                            {
+                                bulletsToAdd = ammoInBoxForAK;
+                                ammoInBoxForAK -= bulletsToAdd;
+                                gunSystem.addAmmo(bulletsToAdd);
+                                Debug.Log($"Пополнено {bulletsToAdd} патрон. Осталось {ammoInBoxForAK}");
+                            }
+                            else
+                            {
+                                ammoInBoxForAK -= bulletsToAdd;
+                                gunSystem.addAmmo(bulletsToAdd);
+                                Debug.Log($"Пополнено {bulletsToAdd} патрон. Осталось {ammoInBoxForAK}");
+                            }
+                        }
                     }
-                    else
+                    else if(gunSystem.name == "870_Shotgun")
                     {
-                        ammoInBox -= bulletsToAdd;
-                        gunSystem.addAmmo(bulletsToAdd);
+                        if(ammoInBoxForShotgun > 0)
+                        {
+                            if (ammoInBoxForShotgun <= bulletsToAdd)
+                            {
+                                bulletsToAdd = ammoInBoxForShotgun;
+                                ammoInBoxForShotgun -= bulletsToAdd;
+                                gunSystem.addAmmo(bulletsToAdd);
+                                Debug.Log($"Пополнено {bulletsToAdd} патрон. Осталось {ammoInBoxForShotgun}");
+                            }
+                            else
+                            {
+                                ammoInBoxForShotgun -= bulletsToAdd;
+                                gunSystem.addAmmo(bulletsToAdd);
+                                Debug.Log($"Пополнено {bulletsToAdd} патрон. Осталось {ammoInBoxForShotgun}");
+                            }
+                        }
                     }
-                    Debug.Log($"Игрок пополнил {bulletsToAdd} патрон. Осталось в ящике: {ammoInBox}");
-                    if (ammoInBox <= 0)
+                    else if(gunSystem.name == "Pistol")
+                    {
+                        if (ammoInBoxForPistol > 0)
+                        {
+                            if (ammoInBoxForPistol <= bulletsToAdd)
+                            {
+                                bulletsToAdd = ammoInBoxForPistol;
+                                ammoInBoxForPistol -= bulletsToAdd;
+                                gunSystem.addAmmo(bulletsToAdd);
+                                Debug.Log($"Пополнено {bulletsToAdd} патрон. Осталось {ammoInBoxForPistol}");
+                            }
+                            else
+                            {
+                                ammoInBoxForPistol -= bulletsToAdd;
+                                gunSystem.addAmmo(bulletsToAdd);
+                                Debug.Log($"Пополнено {bulletsToAdd} патрон. Осталось {ammoInBoxForPistol}");
+                            }
+
+                        }
+
+                    }
+                    if (ammoInBoxForAK <=0 && ammoInBoxForPistol <=0 && ammoInBoxForShotgun <=0)
                     {
                         Destroy(gameObject);
                     }
                 }
                 else
                 {
-                    Debug.Log("В ящике больше нет гранат или у игрока максимальное количество гранат.");
+                    Debug.Log("В ящике больше нет патрон или у игрока максимальное количество патрон.");
                 }
             }
         }
