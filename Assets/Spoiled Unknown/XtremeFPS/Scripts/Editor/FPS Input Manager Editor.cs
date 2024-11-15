25b93bade69468743b3eac193152e47f95a63fb8
StepanKozyrev <stepankozyrev32@gmail.com>
2024-11-15 11:57:32 +0300

    -krasivie_scripti

--- Assets/Spoiled Unknown/XtremeFPS/Scripts/Editor/FPS Input Manager Editor.cs
+++ Assets/Spoiled Unknown/XtremeFPS/Scripts/Editor/FPS Input Manager Editor.cs
@@ -20,37 +20,37 @@ namespace XtremeFPS.Editor
             serInputM_UI = new SerializedObject(inputM_UI);
         }
 
-        public override void OnInspectorGUI()
-        {
-            serInputM_UI.Update();
-            #region Intro
-            EditorGUILayout.Space();
-            GUI.color = Color.black;
-            GUILayout.Label("Xtreme FPS Controller", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-            GUI.color = Color.green;
-            GUILayout.Label("Input Manager Script", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
-            EditorGUILayout.Space();
-            GUI.color = Color.black;
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-            GUI.color = Color.white;
-            #endregion
-#if UNITY_ANDROID || UNITY_IOS
-        inputM_UI.maxTouchLimit = EditorGUILayout.IntField(new GUIContent("Max Touch Limit", "Maximum number of touches that should be handled by the input manager."), inputM_UI.maxTouchLimit);
-        SerializedProperty touchDetectionMode = serializedObject.FindProperty("touchDetectionMode");
-        EditorGUILayout.PropertyField(touchDetectionMode, new GUIContent("Touch Detection Mode", "Determines the mode at which the touch will be calculated."), true);
-        serializedObject.ApplyModifiedProperties();
-#endif
-            #region Update Changes
-            //Sets any changes from the prefab
-            if (GUI.changed)
-            {
-                EditorUtility.SetDirty(inputM_UI);
-                Undo.RecordObject(inputM_UI, "Input Manager Change");
-                serInputM_UI.ApplyModifiedProperties();
-            }
-            #endregion
-        }
+//        public override void OnInspectorGUI()
+//        {
+//            serInputM_UI.Update();
+//            #region Intro
+//            EditorGUILayout.Space();
+//            GUI.color = Color.black;
+//            GUILayout.Label("Xtreme FPS Controller", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
+//            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+//            GUI.color = Color.green;
+//            GUILayout.Label("Input Manager Script", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
+//            EditorGUILayout.Space();
+//            GUI.color = Color.black;
+//            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+//            GUI.color = Color.white;
+//            #endregion
+//#if UNITY_ANDROID || UNITY_IOS
+//        inputM_UI.maxTouchLimit = EditorGUILayout.IntField(new GUIContent("Max Touch Limit", "Maximum number of touches that should be handled by the input manager."), inputM_UI.maxTouchLimit);
+//        SerializedProperty touchDetectionMode = serializedObject.FindProperty("touchDetectionMode");
+//        EditorGUILayout.PropertyField(touchDetectionMode, new GUIContent("Touch Detection Mode", "Determines the mode at which the touch will be calculated."), true);
+//        serializedObject.ApplyModifiedProperties();
+//#endif
+//            #region Update Changes
+//            //Sets any changes from the prefab
+//            if (GUI.changed)
+//            {
+//                EditorUtility.SetDirty(inputM_UI);
+//                Undo.RecordObject(inputM_UI, "Input Manager Change");
+//                serInputM_UI.ApplyModifiedProperties();
+//            }
+//            #endregion
+//        }
 
     }
 }
