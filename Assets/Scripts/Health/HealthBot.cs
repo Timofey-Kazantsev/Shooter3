03aaa18ded1c777d9316e9e42b2af0ac67a2d7e9
TikhonTwinkle <tikhonnnnnn@gmail.com>
2024-11-21 20:13:57 +0300

    Ragdoll
    https://www.youtube.com/watch?v=dbkD0jOwU_0

--- Assets/Scripts/Health/HealthBot.cs
+++ Assets/Scripts/Health/HealthBot.cs
@@ -3,22 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace XtremeFPS.WeaponSystem
-{
-    public class HealthBot : MonoBehaviour
-    {
+namespace XtremeFPS.WeaponSystem {
+    public class HealthBot : MonoBehaviour {
         public float health = 100f;
-        // Start is called before the first frame update
-        void Awake()
-        {
+        void Awake() {
         }
 
-        public void Damage(float damage)
-        {
+        public void Damage(float damage) {
             health -= damage;
-            if (health <= 0)
-            {
-                Destroy(gameObject);
+            if (health <= 0) {
+
+               // Destroy(gameObject);
             }
         }
     }
